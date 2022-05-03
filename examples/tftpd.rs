eafcd8d43869663212678faaebff8adc21f57058
9506hqwy <97424592+9506hqwy@users.noreply.github.com>
2022-05-04 00:47:00 +0900

    バージョンを 0.2.0 に更新した。

--- examples/tftpd.rs
+++ examples/tftpd.rs
@@ -11,7 +11,7 @@ async fn main() -> Result<(), Error> {
     env_logger::init();
 
     let matches = Command::new("TFTP Server")
-        .version("0.1.0")
+        .version("0.2.0")
         .arg(
             Arg::new("bind")
                 .short('i')
